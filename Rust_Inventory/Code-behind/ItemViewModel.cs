a946b9083fdd298d92aeb6e7923275e4e2d25881
Trickje <rick.pijpers@hotmail.com>
2021-12-20 13:05:49 +0100

    initial commit

---
+++ Rust_Inventory/Code-behind/ItemViewModel.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace Rust_Inventory.Code_behind
+{
+    class ItemViewModel : INotifyPropertyChanged
+    {
+        private IList<ItemBase> m_ItemList;
+        public int SelectedInventorySlot;
+        private bool m_Switching;
+        public bool Switching
+        {
+            get
+            {
+                return m_Switching;
+            }
+            set
+            {
+                if(value == false)
+                {
+                    m_SwitchString = "Not swapping";
+                    OnPropertyChanged("SwitchString");
+                }
+                else
+                {
+                    m_SwitchString = "Swapping items";
+                    OnPropertyChanged("SwitchString");
+                }
+                m_Switching = value;
+            }
+        }
+        private string m_SwitchString;
+        public string SwitchString
+        {
+            get
+            {
+                return m_SwitchString;
+            }
+            set
+            {
+                m_SwitchString = value;
+            }
+        }
+        
+        private ItemBase m_CurrentSelectedItem;
+        public ItemBase CurrentSelectedItem {
+            get
+            {
+                return m_CurrentSelectedItem;
+            }
+            set
+            {
+                m_CurrentSelectedItem = value;
+            }
+        }
+        public void UpdateCurrentSelection(int index)
+        {
+            SelectedInventorySlot = index;
+            m_CurrentSelectedItem = Items[SelectedInventorySlot];
+            OnPropertyChanged("CurrentSelectedItem");
+            
+        }
+
+        public void SwapItems(int index1, int index2)
+        {
+            ItemBase temp = Items[index1];
+            Items[index1] = Items[index2];
+            Items[index2] = temp;
+            Switching = false;
+            SwitchString = "Not Swapping";
+            OnPropertyChanged("Items");
+        }
+
+        public ItemViewModel()
+        {
+            SelectedInventorySlot = 0;
+            Switching = false;
+            SwitchString = "Not Swapping";
+            m_ItemList = new List<ItemBase>
+            {
+                new ItemBase{
+                    ItemID= 1,
+                    ItemName ="AK 47",
+                    ItemCount=1,
+                    ItemDamage=120.0f,
+                    ItemSprite="AK_StockImage.jpg",
+                    ItemRangeOfEffect=50.0f,
+                    ItemDurability=100.0f
+                },
+                new ItemBase{
+                    ItemID= 2,
+                    ItemName ="Pistol (Silenced)",
+                    ItemCount=1,
+                    ItemDamage=80.0f,
+                    ItemSprite="Pistol_Silenced.jpg",
+                    ItemRangeOfEffect=30.0f,
+                    ItemDurability=100.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                },
+                new ItemBase{
+                    ItemID= 0,
+                    ItemName ="Empty",
+                    ItemCount=0,
+                    ItemDamage=0.0f,
+                    ItemSprite="Empty.png",
+                    ItemRangeOfEffect=0.0f,
+                    ItemDurability=0.0f
+                }
+            };
+
+            m_CurrentSelectedItem = Items[SelectedInventorySlot];
+        }
+        public IList<ItemBase> Items
+        {
+            get { return m_ItemList; }
+            set { m_ItemList = value; }
+        }
+        #region INotifyPropertyChanged Members  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ Rust_Inventory/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Rust_Inventory.Code_behind;
+
+
+namespace Rust_Inventory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ItemViewModel m_ViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            m_ViewModel = new ItemViewModel();
+            this.DataContext = m_ViewModel;
+            this.Show();
+        }
+        private void OnClick0(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 0);
+            }
+            m_ViewModel.UpdateCurrentSelection(0);
+        }
+        private void OnClick1(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 1);
+            }
+            m_ViewModel.UpdateCurrentSelection(1);
+        }
+        private void OnClick2(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 2);
+            }
+            m_ViewModel.UpdateCurrentSelection(2);
+        }
+        private void OnClick3(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 3);
+            }
+            m_ViewModel.UpdateCurrentSelection(3);
+        }
+        private void OnClick4(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 4);
+            }
+            m_ViewModel.UpdateCurrentSelection(4);
+        }
+        private void OnClick5(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 5);
+            }
+            m_ViewModel.UpdateCurrentSelection(5);
+        }
+        private void OnClick6(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 6);
+            }
+            m_ViewModel.UpdateCurrentSelection(6);
+        }
+        private void OnClick7(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 7);
+            }
+            m_ViewModel.UpdateCurrentSelection(7);
+        }
+        private void OnClick8(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.Switching)
+            {
+                m_ViewModel.SwapItems(m_ViewModel.SelectedInventorySlot, 8);
+            }
+            m_ViewModel.UpdateCurrentSelection(8);
+        }
+        private void OnSwitchStart(object sender, RoutedEventArgs e)
+        {
+            m_ViewModel.Switching = !m_ViewModel.Switching;
+        }
+    }
+}
